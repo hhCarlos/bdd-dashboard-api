18e03b5d04b5c005829659da5163a3b67ba10a62
Carlos Enrique Hernandez Hernandez <hernandez.enrique.carlos@gmail.com>
2025-11-07 14:20:16 -0600

    feat: primer commit con solo el esqueleto

---
+++ src/main/java/com/bdd_dashboard_api/bdd_dashboard_api/BddDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.bdd_dashboard_api.bdd_dashboard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddDashboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddDashboardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdd_dashboard_api/bdd_dashboard_api/BddDashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdd_dashboard_api.bdd_dashboard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddDashboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
